aa0783785e3bfb866db1f4eb3098115a4490c2df
Nainsi.Singh <nainsi.singh@sita.aero>
2025-01-17 15:49:11 +0530

    First Commit

---
+++ src/main/java/com/springdatajpa/springboot/entity/Product.java
@@ -0,0 +1,27 @@
+package com.springdatajpa.springboot.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+/*@Getter
+@Setter*/
+public class Product {
+    @Id
+    private long id;
+    private String sku;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private boolean active;
+    private String imageUrl;
+    private LocalDateTime dateCreated;
+    private LocalDateTime lastUpdated;
+
+
+}
